76d5c32cf0aaded715085398cb49d4f7f7a43755
arturschaefer <artur.schaefer2@gmail.com>
2016-11-21 15:19:28 -0200

    Teste de inserção de pessoa e endereço

--- src/cgt/TesteHibernate.java
+++ src/cgt/TesteHibernate.java
@@ -5,9 +5,12 @@
  */
 package cgt;
 
+import cdp.Endereco;
+import cdp.Pessoa;
 import cgd.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -22,6 +25,28 @@ public class TesteHibernate {
         SessionFactory sf = HibernateUtil.getSessionFactory();
         Session sessao = sf.openSession();
         
+        Pessoa pes = new Pessoa();
+        Endereco endereco = new Endereco();
+        
+        endereco.setBairro("centro");
+        endereco.setCep(29725000);
+        endereco.setCidade("Marilândia");
+        endereco.setRua("Virginia Paganini Tozzi");
+        
+        pes.setCpf("12156544727");
+        pes.setEmail("batata");
+        pes.setNome("Artur");
+        pes.setEndereco(endereco);
+        pes.setTelefone01(000000);
+        pes.setTelefone02(111111);
+        
+        Transaction ts = sessao.beginTransaction();
+        
+        sessao.save(endereco);
+        sessao.save(pes);
+        
+        ts.commit();
+        sessao.flush();
         sessao.close();                 
     }
     
