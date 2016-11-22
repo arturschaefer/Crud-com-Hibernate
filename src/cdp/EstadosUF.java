6b04fe54cdd30006dc177a8fecebcb4b73d1643d
arturschaefer <artur.schaefer2@gmail.com>
2016-11-22 20:57:25 -0200

    enum de UF

---
+++ src/cdp/EstadosUF.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cdp;
+
+import javax.persistence.Embeddable;
+
+/*
+ * Autor:           Artur Schaefer <artur.schaefer2 at gmail.com>
+ * Data:            22/11/2016
+ * Hora:            19:57:13
+ * Codificação:     UTF-8
+ * Disciplina:      Programação Orientada a Objetos II
+ * Professor:       Jean 
+ * Instituição:     Instituto Federal de Educação, Ciência e Tecnologia do Espírito Santo - IFES
+ *
+ * Nome Original:       EstadosUF
+ * Pacote de Criação:   cdp
+ */
+
+@Embeddable
+public enum EstadosUF { ES, MG, SP, RJ; }
