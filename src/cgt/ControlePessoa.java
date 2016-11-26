548633f01548259f687431c96ea4cb848f91267d
arturschaefer <artur.schaefer2@gmail.com>
2016-11-26 12:22:21 -0200

    Listar todas pessoas != Procurar Pessoa

--- src/cgt/ControlePessoa.java
+++ src/cgt/ControlePessoa.java
@@ -5,10 +5,7 @@ import cdp.Endereco;
 import cdp.Pessoa;
 import cgd.PessoaDAO;
 import java.sql.SQLException;
-import java.util.Iterator;
 import java.util.List;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 /*
  * Autor:           aschaefer
@@ -23,23 +20,24 @@ import javax.swing.table.DefaultTableModel;
  * Pacote de Criação:   cgd
  */
 public class ControlePessoa {
+
     PessoaDAO pesDAO;
 
     public ControlePessoa() {
         pesDAO = new PessoaDAO();
     }
 
-    public int inserirPessoa(String nome, String cpf, int telefone01, int telefone02, 
-            Endereco endereco, Conta conta, String observacoes, 
-            String rg ) throws SQLException, ClassNotFoundException {
+    public int inserirPessoa(String nome, String cpf, int telefone01, int telefone02,
+            Endereco endereco, Conta conta, String observacoes,
+            String rg) throws SQLException, ClassNotFoundException {
 
-        Pessoa pes = new Pessoa(nome, cpf, rg, telefone01, telefone02, 
+        Pessoa pes = new Pessoa(nome, cpf, rg, telefone01, telefone02,
                 observacoes, endereco, conta);
-        
         pesDAO.inserir(pes);
         return pes.getCod();
     }
-/*
+
+    /*
     public int alterarVeiculo(int id, String placa, String modelo, String marca, int ano, String cor,
             double diaria) throws ClassNotFoundException, SQLException {
 
@@ -54,38 +52,23 @@ public class ControlePessoa {
 
         pesDAO.excluir(veic);
     }
-    */
-    
+     */
+
     //SERÁ QUE PODE ISSO, UMA JTABLE NO CONTROLE?
     //FAZ UM POUCO DE SENTIDO PORQUE É O CONTROLE QUE ALTERA A VISÃO, MAS AO MESMO TEMPO É ESTRANHO
     //PORQUE O CONTROLE ERA PARA PASSAR SOMENTE DADOS E NÃO VISUAL
-    public void listarPessoas(int tipo, String pesq, JTable tabela) throws SQLException {
+    public List<Pessoa> listarPessoas(int tipo, String pesq) throws SQLException {
 
         List<Pessoa> lista = null;
-
-        // Apagar linhas da TABELA
-        ((DefaultTableModel) tabela.getModel()).setRowCount(0);
-
         switch (tipo) {
             case 0:
-                lista = pesDAO.pesquisarNome(pesq);
-                break; 
+                lista = pesDAO.listarTodos();
+                break;
             case 1:
                 lista = pesDAO.procuraPessoa(pesq);
                 break;
         }
 
-        if (lista == null) {
-
-        } else {
-            Pessoa pes;
-            Iterator<Pessoa> it = lista.iterator();
-            while (it.hasNext()) {
-                pes = it.next();
-                ((DefaultTableModel) tabela.getModel()).addRow(pes.toArray());
-            }
-
-        }
-
+        return lista;
     }
 }
