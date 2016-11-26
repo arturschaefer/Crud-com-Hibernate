5ebb083f6bd335435eec5109f7028ca2f6e22e7d
Artur <Artur@i3-casa>
2016-11-26 16:47:50 -0200

    Criado para gerência de conta

---
+++ src/cgt/ControleConta.java
@@ -0,0 +1,47 @@
+package cgt;
+
+import cdp.Conta;
+import cdp.Pessoa;
+import cgd.ContaDAO;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Artur
+ */
+public class ControleConta {
+    ContaDAO contaDAO;
+
+    public ControleConta() {
+        contaDAO = new ContaDAO();
+    }
+
+    public int inserirConta(Conta cont) throws SQLException, ClassNotFoundException {
+        contaDAO.inserir(cont);
+        return cont.getIdConta();
+    }
+
+    public int alterarConta(Conta cont) throws ClassNotFoundException, SQLException {
+        contaDAO.alterar(cont);
+        return cont.getIdConta();
+    }
+
+    public void excluirVeiculo(Conta cont) throws SQLException {
+        contaDAO.excluir(cont);
+    }
+
+    public List<Conta> listarContas(int tipo, String pesq) throws SQLException {
+        List<Conta> lista = null;
+        switch (tipo) {
+            case 0:
+                lista = contaDAO.listarTodos();
+                break;
+            case 1:
+                lista = contaDAO.procuraPorNome(Pessoa.class, "cpf", pesq);
+                break;
+        }
+
+        return lista;
+    }
+}
